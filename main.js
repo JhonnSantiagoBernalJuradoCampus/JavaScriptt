bf8033d9671f8bcf9ff9f5175ae1be9116076c7f
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-09 21:30:27 -0500

    feat: :sparkles: Condiciones por tipo de nota
    Agregue los condicionales para tener en cuenta en que tipo de calificacion se iba a realizar la conversion

--- main.js
+++ main.js
@@ -24,11 +24,11 @@ const notasTraditional = {
         "F"
     ],
     sbgRating : [
+        "5",
         "4",
         "3",
         "2",
         "1",
-        "0",
         "0"
     ],
     proficiencyLevelWithStandard : [
@@ -40,33 +40,89 @@ const notasTraditional = {
         "No attempt made"
     ]
 }
-console.log(notasTraditional)
+
 function transfrom(){
+    let tipo = prompt("Ingrese en que tipo de calificacion desea ingresar\n\t1.Traditional\n\t2.Point\n\t3.SBG")
     let mensaje = "El equivalente de su nota en el Standar Grading System es"
-    let nota = prompt("Ingrese su nota");
-    if (nota >= 90 && nota <= 100 || nota >= 12 && nota < 15 || nota == "A" || nota == "a") {
-        console.log(`${mensaje} 4, Felicitaciones ahora eres un Sputnik`);
+    if(tipo == 1){
+        let nota = prompt("Ingrese su nota entre\n0-100");
+        if (nota >= 90 && nota <= 100){
+            alert(`${mensaje} A, Felicitaciones ahora eres un Sputnik`);
+        }
+        else if (nota >= 80 && nota < 90){
+            alert(`${mensaje} B, Felicitaciones te ganaste la beca en campus`);
+        }
+        else if (nota >= 70 && nota < 80){
+            alert(`${mensaje} C, Agradecemos por tu participación pero no has sido seleccionado preparate mejor para el proximo mes`);
+        }
+        else if (nota >= 60 && nota < 70){
+            alert(`${mensaje} D, Lo sentimos no has sido seleccionado, preparate mejor para que vuelvas a realizar las pruebas en agosto`);
+        }
+        else if (nota >0 && nota < 60){
+            alert(`${mensaje} E, Dedicate a otra cosa o vuelve en un año`);
+        }
+        else if (nota == 0){
+            alert(`${mensaje} F, Dediquese a otra cosa, buen día`);
+        }
+        else {
+            console.log(`Porfavor ingrese datos que correspondan a una nota en los estandares`);
+            transfrom()
+        }
     }
-    else if (nota >= 80 && nota < 90 || nota >= 9 && nota < 12 || nota == "B"|| nota == "b") {
-        console.log(`${mensaje} 3, Felicitaciones te ganaste la beca en campus`);
+    else if (tipo==2){
+        let nota = prompt("Ingrese su nota entre\n0-14");
+        if (nota >= 12 && nota < 15){
+            alert(`${mensaje} A, Felicitaciones ahora eres un Sputnik`);
+        }
+        else if (nota >= 9 && nota < 12){
+            alert(`${mensaje} B, Felicitaciones te ganaste la beca en campus`);
+        }
+        else if (nota >= 6 && nota < 9){
+            alert(`${mensaje} C, Agradecemos por tu participación pero no has sido seleccionado preparate mejor para el proximo mes`);
+        }
+        else if (nota >= 3 && nota < 6){
+            alert(`${mensaje} D, Lo sentimos no has sido seleccionado, preparate mejor para que vuelvas a realizar las pruebas en agosto`);
+        }
+        else if (nota >= 1 && nota < 39){
+            alert(`${mensaje} E, Dedicate a otra cosa o vuelve en un año`);
+        }
+        else if (nota == 0){
+            alert(`${mensaje} F, Dediquese a otra cosa, buen día`);
+        }
+        else {
+            console.log(`Porfavor ingrese datos que correspondan a una nota en los estandares`);
+            transfrom()
+        }
     }
-    else if (nota >= 70 && nota < 80 || nota >= 6 && nota < 9 || nota == "C"|| nota == "c") {
-        console.log(`${mensaje} 2, Agradecemos por tu participación pero no has sido seleccionado preparate mejor para el proximo mes`);
-    }
-    else if (nota >= 60 && nota < 70 || nota >= 3 && nota < 6 || nota == "D"|| nota == "d") {
-        console.log(`${mensaje} 1, Lo sentimos no has sido seleccionado, preparate mejor para que vuelvas a realizar las pruebas en agosto`);
-    }
-    else if (nota >=15 && nota < 60 || nota >= 1 && nota < 3 || nota == "E"|| nota == "e") {
-        console.log(`${mensaje} 0, Dedicate a otra cosa o vuelve en un año`);
-    }
-    else if (nota == 0 || nota == "F" || nota =="f"){
-        console.log(`${mensaje} 0, Dediquese a otra cosa, buen día`);
+    else if(tipo==3){
+        let nota = prompt("Ingrese su nota entre\n0-5");
+
+        if (nota == 5){
+            alert(`${mensaje} A, Felicitaciones ahora eres un Sputnik`);
+        }
+        else if (nota == 4){
+            alert(`${mensaje} B, Felicitaciones te ganaste la beca en campus`);
+        }
+        else if (nota == 3){
+            alert(`${mensaje} C, Agradecemos por tu participación pero no has sido seleccionado preparate mejor para el proximo mes`);
+        }
+        else if (nota == 2){
+            alert(`${mensaje} D, Lo sentimos no has sido seleccionado, preparate mejor para que vuelvas a realizar las pruebas en agosto`);
+        }
+        else if (nota == 1){
+            alert(`${mensaje} E, Dedicate a otra cosa o vuelve en un año`);
+        }
+        else if (nota == 0){
+            alert(`${mensaje} F, Dediquese a otra cosa, buen día`);
+        }
+        else {
+            console.log(`Porfavor ingrese datos que correspondan a una nota en los estandares`);
+            transfrom()
+        }
     }
     else {
         console.log(`Porfavor ingrese datos que correspondan a una nota en los estandares`);
         transfrom()
     }
-
-
 }
 transfrom()
